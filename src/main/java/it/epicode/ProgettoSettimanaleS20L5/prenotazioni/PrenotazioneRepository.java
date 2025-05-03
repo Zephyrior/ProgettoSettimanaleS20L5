a676750cda6131727ae7e86b72108424a40cb5e5
Zephyrior <mark.temblique@gmail.com>
2025-05-03 14:55:58 +0200

    cancellazione Prenotazione Repository

--- src/main/java/it/epicode/ProgettoSettimanaleS20L5/prenotazioni/PrenotazioneRepository.java
+++
@@ -1,7 +0,0 @@
-package it.epicode.ProgettoSettimanaleS20L5.prenotazioni;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
-}
